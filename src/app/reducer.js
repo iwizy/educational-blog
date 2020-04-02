4961697f0eb1e94d4775fe2380277043223b0445
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-04-02 17:16:38 +0300

    App reducer for user page

--- src/app/reducer.js
+++ src/app/reducer.js
@@ -7,11 +7,16 @@ export default function appReducer(state = initState, action) {
   switch (action.type) {
     case 'SIGN-IN_SUCCESS':
     case 'SIGN-UP_SUCCESS':
-    case 'USER_PAGE_GET_DATA_SUCCESS':
     case 'APPLICATION-AUTH':
       return {
         ...state,
         user: action.payload
+      };
+
+    case 'USER_PAGE_GET_DATA_SUCCESS':
+      return {
+        ...state,
+        profile: action.payload
       };
     case 'APPLICATION-SIGN-OUT':
       return {
