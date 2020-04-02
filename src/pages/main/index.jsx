1a7ae661eb62b9d86bdcf6b465a499991dae2e3d
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-04-02 22:21:06 +0300

    Main page grid removed

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import * as Actions from './actions';
 import Title from "src/components/title";
 import {Item, Icon, Grid} from 'semantic-ui-react'
@@ -17,62 +17,54 @@ class Main extends Component {
   };
 
   onScroll = (e) => {
-    const { posts, isLoadingPosts } = this.props;
+    const {posts, isLoadingPosts} = this.props;
     const postsLength = posts.length;
     const windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
 
-    if(windowRelativeBottom <= document.documentElement.clientHeight + 100 && !isLoadingPosts) {
+    if (windowRelativeBottom <= document.documentElement.clientHeight + 100 && !isLoadingPosts) {
       this.props.getScrollPostsAction(postsLength)
     }
   };
 
   onClickLike = (evt) => {
-    const { id } = evt.target;
+    const {id} = evt.target;
     this.props.increaseLikeCountAction(id);
   };
 
   onClickDislike = (evt) => {
-    const { id } = evt.target;
+    const {id} = evt.target;
     this.props.increaseDislikeCountAction(id);
   };
 
   render() {
-    const { posts } = this.props;
+    const {posts} = this.props;
     return (
       <>
         <Title title='Блог: Главная страница'/>
-        <Grid celled='internally'>
-          <Grid.Row>
-            <Grid.Column width={3}></Grid.Column>
-            <Grid.Column width={10}>
-              <Item.Group divided>
-                {posts.map((postItem) => {
-                  return (
-                    <Item key={postItem.id}>
-                      <Item.Content>
-                        <Item.Header as={Link} to={`/post/${postItem.id}`}>{postItem.title}</Item.Header>
-                        <Item.Description>
-                          {postItem.content}
-                        </Item.Description>
-                        <Item.Extra>
-                          <span className={style.extraItem}>Автор: {postItem.author.login}</span>
-                          <span className={style.extraItem}><Icon color='green' name='eye'/>{postItem.viewsCount}</span>
-                          <span onClick={this.onClickLike} className={style.extraItem}><Icon id={postItem.id}
-                                                                                             color='green'
-                                                                                             name='thumbs up outline'/>{postItem.likesCount}</span>
-                          <span onClick={this.onClickDislike} className={style.extraItem}><Icon id={postItem.id}
-                                                                                                color='red'
-                                                                                                name='thumbs down outline'/>{postItem.dislikesCount}</span>
-                        </Item.Extra>
-                      </Item.Content>
-                    </Item>
-                  );
-                })}
-              </Item.Group>
-            </Grid.Column>
-            <Grid.Column width={3}></Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <Item.Group divided>
+          {posts.map((postItem) => {
+            return (
+              <Item key={postItem.id}>
+                <Item.Content>
+                  <Item.Header as={Link} to={`/post/${postItem.id}`}>{postItem.title}</Item.Header>
+                  <Item.Description>
+                    {postItem.content}
+                  </Item.Description>
+                  <Item.Extra>
+                    <span className={style.extraItem}>Автор: {postItem.author.login}</span>
+                    <span className={style.extraItem}><Icon color='green' name='eye'/>{postItem.viewsCount}</span>
+                    <span onClick={this.onClickLike} className={style.extraItem}><Icon id={postItem.id}
+                                                                                       color='green'
+                                                                                       name='thumbs up outline'/>{postItem.likesCount}</span>
+                    <span onClick={this.onClickDislike} className={style.extraItem}><Icon id={postItem.id}
+                                                                                          color='red'
+                                                                                          name='thumbs down outline'/>{postItem.dislikesCount}</span>
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+            );
+          })}
+        </Item.Group>
       </>
     );
   }
