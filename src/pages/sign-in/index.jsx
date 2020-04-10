7d7a9c0a5ad93655f2c5e01f2a5b6e59fb2e602b
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-04-11 00:54:16 +0300

    Login form styles

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Button, Segment, Form} from 'semantic-ui-react';
+import {Button, Segment, Form, Grid, Header} from 'semantic-ui-react';
 
 import PropTypes from 'prop-types';
 import Input from 'src/components/input';
 import Title from 'src/components/title';
 import * as Actions from './actions';
 
-import style from 'src/app/style.css';
+import style from './style.css';
 
 class SignIn extends Component {
   static propTypes = {
@@ -25,32 +25,44 @@ class SignIn extends Component {
     return (
       <>
         <Title title='Блог: Войти на сайт'/>
-        <Segment raised size='small'>
-          <Form>
-            <div>
-              <div className={style.fieldWrapper}>
-                <Input
-                  id="login"
-                  value={this.props.dataForm.login}
-                  onChange={this.props.changeFieldAction}
-                  placeholder='Логин'
-                />
-              </div>
-              <div className={style.fieldWrapper}>
-                <Input
-                  id="password"
-                  value={this.props.dataForm.password}
-                  onChange={this.props.changeFieldAction}
-                  placeholder='Пароль'
-                  type='password'
-                />
-              </div>
-              <div>
-                <Button positive onClick={this.onSubmit}>Войти</Button>
-              </div>
-            </div>
-          </Form>
-        </Segment>
+        <Header as='h2'>Войти на сайт</Header>
+        <Grid textAlign='center'>
+          <Grid.Column style={{maxWidth: 450}}>
+            <Segment raised size='small'>
+              <Form>
+                <div>
+                  <div className={style.fieldWrapper}>
+                    <Input
+                      fluid
+                      icon='user'
+                      iconPosition='left'
+                      id="login"
+                      value={this.props.dataForm.login}
+                      onChange={this.props.changeFieldAction}
+                      placeholder='Логин'
+                      className={style.inputFields}
+                    />
+                  </div>
+                  <div className={style.fieldWrapper}>
+                    <Input
+                      fluid
+                      icon='lock'
+                      iconPosition='left'
+                      id="password"
+                      value={this.props.dataForm.password}
+                      onChange={this.props.changeFieldAction}
+                      placeholder='Пароль'
+                      type='password'
+                    />
+                  </div>
+                  <div>
+                    <Button positive onClick={this.onSubmit}>Войти</Button>
+                  </div>
+                </div>
+              </Form>
+            </Segment>
+          </Grid.Column>
+        </Grid>
       </>
     );
   }
