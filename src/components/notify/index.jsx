dfd4a81fe34266af592ab9b39f9f3ee141e8cb59
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-04-12 00:11:33 +0300

    Notification component style

--- src/components/notify/index.jsx
+++ src/components/notify/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Message } from 'semantic-ui-react';
+import style from './style.css';
 
 class Information extends Message {
   render() {
     const { header, content, color, hidden } = this.props;
     return (
-      <div>
+      <div className={style.notify}>
         <Message
           header={header}
           content={content}
