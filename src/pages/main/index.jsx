2b8ae3eff961e9bbc77bb8d007576120d955e68b
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-04-14 23:39:48 +0300

    Add loader to main page

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import * as Actions from './actions';
 import Title from "src/components/title";
-import {Item, Icon, Header} from 'semantic-ui-react'
+import {Item, Icon, Header, Dimmer, Loader} from 'semantic-ui-react'
 import style from './style.css';
 
 class Main extends Component {
@@ -42,30 +42,36 @@ class Main extends Component {
       <>
         <Title title='Блог: Главная страница'/>
         <Header as='h2'>Последние записи</Header>
-        <Item.Group divided>
-          {posts.map((postItem) => {
-            return (
-              <Item key={postItem.id}>
-                <Item.Content>
-                  <Item.Header as={Link} to={`/post/${postItem.id}`}>{postItem.title}</Item.Header>
-                  <Item.Description>
-                    {postItem.content}
-                  </Item.Description>
-                  <Item.Extra>
-                    <span className={style.extraItem}>Автор: {postItem.author.login}</span>
-                    <span className={style.extraItem}><Icon color='green' name='eye'/>{postItem.viewsCount}</span>
-                    <span onClick={this.onClickLike} className={style.extraItem}><Icon id={postItem.id}
-                                                                                       color='green'
-                                                                                       name='thumbs up outline'/>{postItem.likesCount}</span>
-                    <span onClick={this.onClickDislike} className={style.extraItem}><Icon id={postItem.id}
-                                                                                          color='red'
-                                                                                          name='thumbs down outline'/>{postItem.dislikesCount}</span>
-                  </Item.Extra>
-                </Item.Content>
-              </Item>
-            );
-          })}
-        </Item.Group>
+        {posts
+          ? <Item.Group divided>
+            {posts.map((postItem) => {
+              return (
+                <Item key={postItem.id}>
+                  <Item.Content>
+                    <Item.Header as={Link} to={`/post/${postItem.id}`}>{postItem.title}</Item.Header>
+                    <Item.Description>
+                      {postItem.content}
+                    </Item.Description>
+                    <Item.Extra>
+                      <span className={style.extraItem}>Автор: {postItem.author.login}</span>
+                      <span className={style.extraItem}><Icon color='green' name='eye'/>{postItem.viewsCount}</span>
+                      <span onClick={this.onClickLike} className={style.extraItem}><Icon id={postItem.id}
+                                                                                         color='green'
+                                                                                         name='thumbs up outline'/>{postItem.likesCount}</span>
+                      <span onClick={this.onClickDislike} className={style.extraItem}><Icon id={postItem.id}
+                                                                                            color='red'
+                                                                                            name='thumbs down outline'/>{postItem.dislikesCount}</span>
+                    </Item.Extra>
+                  </Item.Content>
+                </Item>
+              );
+            })}
+          </Item.Group>
+          :
+          <Dimmer active>
+            <Loader>Загрузка</Loader>
+          </Dimmer>
+        }
       </>
     );
   }
